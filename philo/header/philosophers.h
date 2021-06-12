c59662eb0378bd3c1b2edb10cc8f52eb064a3fd4
Abdellah El azraoui <ael-azra@e1r3p4.1337.ma>
2021-06-12 17:34:34 +0100

    first push of my philo project on 1337

---
+++ philo/header/philosophers.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-azra <ael-azra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/11 18:35:40 by ael-azra          #+#    #+#             */
+/*   Updated: 2021/06/12 17:26:26 by ael-azra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct	s_input
+{
+	int	number_of_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_time_to_eat;
+}				t_input;
+
+#endif
