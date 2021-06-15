78060073a976e5186e1ba5fbb7f13bf4cfb5bf7e
Abdellah El azraoui <ael-azra@e2r5p3.1337.ma>
2021-06-15 19:23:23 +0100

    adding threads to my code

--- philo/file/main.c
+++ philo/file/main.c
@@ -6,15 +6,36 @@
 /*   By: ael-azra <ael-azra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/11 18:34:37 by ael-azra          #+#    #+#             */
-/*   Updated: 2021/06/15 12:51:29 by ael-azra         ###   ########.fr       */
+/*   Updated: 2021/06/15 16:42:46 by ael-azra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/philosophers.h"
 
+void*	philo_situation(void *input)
+{
+	printf("%d\n", ((t_input *)input)->number_of_philo);
+	usleep(100);
+	return (void*)1;
+}
+
 void	threads_philo(t_input *input)
 {
-	// write code here !
+	pthread_t	*t_philo;
+	int			i;
+
+	t_philo = (pthread_t *)malloc(sizeof(pthread_t) * input->number_of_philo);
+	while (1)
+	{
+		i = 0;
+		while (i < input->number_of_philo)
+			pthread_create(&t_philo[i++], NULL, &philo_situation, input);
+		i = 0;
+		while (i < input->number_of_philo)
+			pthread_join(t_philo[i++], NULL);
+		break ;
+	}
+	free(t_philo);
 }
 
 int	main(int ac, char *av[])
@@ -32,6 +53,7 @@ int	main(int ac, char *av[])
 		else
 		{
 			threads_philo(input);
+			// system("leaks philo");
 			free(input);
 			return (0);
 		}
