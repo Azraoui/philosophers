376e95c3f3c78e15aa371b26dc99811e2c17d921
Abdellah El azraoui <ael-azra@e2r5p4.1337.ma>
2021-06-17 20:25:26 +0100

    remove global variable

--- philo/file/main.c
+++ philo/file/main.c
@@ -6,26 +6,23 @@
 /*   By: ael-azra <ael-azra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/11 18:34:37 by ael-azra          #+#    #+#             */
-/*   Updated: 2021/06/16 14:42:42 by ael-azra         ###   ########.fr       */
+/*   Updated: 2021/06/17 20:24:49 by ael-azra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/philosophers.h"
 
-pthread_mutex_t mutex;
-int	i;
-
-void*	philo_situation(void *var)
+void	*philo_situation(void *var)
 {
 	t_variables *tmp;
 
 	tmp = (t_variables *)var;
-	pthread_mutex_lock(&mutex);
+	pthread_mutex_lock(&tmp->mutex);
 	if (tmp->position == 0)
 	{
 		
 	}
-	pthread_mutex_unlock(&mutex);
+	pthread_mutex_unlock(&tmp->mutex);
 	return ((void*)1);
 }
 
@@ -34,7 +31,7 @@ void	threads_philo(t_input *input)
 	pthread_t	*t_philo;
 	t_variables	var;
 
-	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_init(&var.mutex, NULL);
 	var.forks = (char *)malloc(sizeof(char) * (input->number_of_philo + 1));
 	var.forks = memset(var.forks, '0', input->number_of_philo);
 	t_philo = (pthread_t *)malloc(sizeof(pthread_t) * input->number_of_philo);
@@ -52,7 +49,7 @@ void	threads_philo(t_input *input)
 			pthread_join(t_philo[var.position++], NULL);
 		break ;
 	}
-	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&var.mutex);
 	free(t_philo);
 }
 
